c1ade2fa2c38dc93ba8fe9f0b6d3cd5d0531c245
MKarthik1729 <karthik.m20@iiits.in>
2025-02-11 14:20:13 +0530

    profile issue in production

--- client/src/components/Profile/Profile.jsx
+++ client/src/components/Profile/Profile.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { useEffect, useState } from 'react'
-import styles from './Profile.module.scss'
+import styles from './profile.module.scss'
 import axios from 'axios'
 import Event from './Event'
 
